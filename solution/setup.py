999ee25ce7958d5cba7ed3674b148120b4afd810
Ales Kirsch <kirschales@gmail.com>
2023-06-03 14:30:48 +0200

    Module name is now correctly inputted

--- solution/setup.py
+++ solution/setup.py
@@ -5,9 +5,7 @@ PROGRAM_NAME = "program"
 setup(
     name=PROGRAM_NAME,
     version="1.0.0",
-    # I belive that this should be string
-    # will either fix or be proven wrong
-    py_modules=[solution],
+    py_modules=["solution"],
     entry_points={
         "console_scripts": [
             f"{PROGRAM_NAME}=solution:main",
