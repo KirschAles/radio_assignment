ebd3096010fece2282234f7eaf4dcc0874b027be
Ales Kirsch <kirschales@gmail.com>
2023-06-04 00:12:33 +0200

    Print now all files

--- solution/solution.py
+++ solution/solution.py
@@ -3,12 +3,22 @@ from argparse import ArgumentParser
 from argparse import Namespace
 from os import environ
 from os import getcwd
+import os
+import json
 
 
 PROGRAM_NAME = 'program'
 VERSION = '1.0.0'
 OUTPUT_ENV = 'TARGET_DIRECTORY'
 INPUT_ENV = 'SOURCE_DIRECTORY'
+
+DATE = 'date'
+
+
+def get_target(filename: str) -> str:
+    index = filename.find('-') + 1
+    new_name = filename[index:]
+    return new_name
 
 
 def get_file_name(inputted: str, env_name: str) -> str:
@@ -36,9 +46,19 @@ def main() -> int:
         print(PROGRAM_NAME, VERSION)
         return 0
 
-    input_name = get_file_name(args.input, INPUT_ENV)
-    output_name = get_file_name(args.output, OUTPUT_ENV)
+    input_dir = get_file_name(args.input, INPUT_ENV)
+    output_dir = get_file_name(args.output, OUTPUT_ENV)
+    is_writing = args.write
 
+    files = os.listdir(input_dir)
+    print(f'Processing {len(files)} files...')
+    for filename in files:
+        directory = {}
+        file_path = str(os.path.join(input_dir, filename))
+        directory['source'] = file_path
+        output_name = str(os.path.join(output_dir, get_target(filename)))
+        directory['target'] = output_name
+        print(directory)
 
     return 0
 
