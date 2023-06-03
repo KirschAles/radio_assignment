e9b1fe9b1bfc4244adf657312687cd502c2512fc
Ales Kirsch <kirschales@gmail.com>
2023-06-03 17:32:49 +0200

    Added parsing of input and output files

--- solution/solution.py
+++ solution/solution.py
@@ -1,9 +1,23 @@
 from sys import argv
 from argparse import ArgumentParser
+from os import environ
+from os import getcwd
 
 
 PROGRAM_NAME = 'program'
 VERSION = '1.0.0'
+OUTPUT_ENV = 'TARGET_DIRECTORY'
+INPUT_ENV = 'SOURCE_DIRECTORY'
+
+
+def get_file_name(inputted, env_name):
+    if inputted is not None:
+        return inputted
+    name = environ.get(env_name)
+    if name is not None:
+        return name
+
+    return getcwd()
 
 
 def parse_input():
@@ -20,6 +34,11 @@ def main():
     if args.version:
         print(PROGRAM_NAME, VERSION)
         return 0
+
+    input_name = get_file_name(args.input, INPUT_ENV)
+    output_name = get_file_name(args.output, OUTPUT_ENV)
+
+
     return 0
 
 if __name__ == "__main__":
