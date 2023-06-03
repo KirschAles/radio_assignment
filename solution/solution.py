cdbc5cc7d60e33d94c770946cafa43686bac05be
Ales Kirsch <kirschales@gmail.com>
2023-06-03 16:26:59 +0200

    Added input parsing

--- solution/solution.py
+++ solution/solution.py
@@ -1,8 +1,19 @@
+from sys import argv
+from argparse import ArgumentParser
+
+
+def parse_input():
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input', help="Directory, from which is input taken.")
+    parser.add_argument('-o', '--output', help="Directory, where to put output.")
+    parser.add_argument('--version', '-v', action='store_true', help="Prints the version of the program.")
+    parser.add_argument('--write', '-w', action='store_true', help="Makes the operation for real.")
+    return parser.parse_args()
 
 
 def main():
-    return 0
-
+    args = parse_input()
+    
 
 if __name__ == "__main__":
     main()
